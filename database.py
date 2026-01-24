80d76793db1b3fed15b325a18a03c76d26954bad
Edu-Guedes <eduardaguedes062007@gmail.com>
2026-01-24 19:02:54 -0300

    cad

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import asyncpg
 async def get_db_connection():
     return await asyncpg.connect(
         user= "postgres",
-        senha="",
+        senha="1234",
         database="blog_pequenos_artistas",
         host="localhost",
         port = 5432
