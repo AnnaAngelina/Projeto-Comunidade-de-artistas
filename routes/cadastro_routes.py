84b12f4575dd55c7538d3238cb9aaade8f57c17e
Edu-Guedes <eduardaguedes062007@gmail.com>
2026-01-24 21:32:52 -0300

    cadastro_OK

--- routes/cadastro_routes.py
+++ routes/cadastro_routes.py
@@ -1,13 +1,28 @@
 import asyncpg
 from database import get_db_connection
 from models import Usuario
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
 
 cadastro = APIRouter(prefix='/cadastro', tags=["cadastro"])
+
+# verifica se banco com o email. fetchrow retorna None se não existe ou algo se existe.
+async def usuario_ja_existe(conn, email: str):
+    query = "SELECT 1 FROM usuario WHERE email = $1" #verifica se existe pelo menos um registro
+    resultado = await conn.fetchrow(query, email)
+    return resultado is not None
 
 @cadastro.post('/criar')
 async def criar(usuario: Usuario):
     conn = await get_db_connection()
+
+    # verifica se já existe
+    if await usuario_ja_existe(conn, usuario.email):
+        await conn.close()
+        raise HTTPException( #interrompe imediatamente a execução da função
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="Usuário já cadastrado com esse email"
+        )
 
     await conn.execute(
         """
